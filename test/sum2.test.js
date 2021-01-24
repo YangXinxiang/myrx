c2ceee4544a07975d1b4f26cdf14c584b5a52c24
yangxinxiang <yangxinxiang@100tal.com>
2021-01-24 16:41:02 +0800

    feat: 测试不同的数据加法; 添加异步处理逻辑,设置超时时间: jest.setTimeout

--- test/sum2.test.js
+++ test/sum2.test.js
@@ -1,4 +1,4 @@
-import {range} from "rxjs"
+import {range, of, timer} from "rxjs"
 import { sum2 } from '../src/base/chapters/chapter-12/sum2';
 
 describe('index', () => {
@@ -25,9 +25,56 @@ describe('index', () => {
           console.log(`sum2 test, in complete~~`);
         }
       )
-    })
+    });
 
-    
+    it("测试字符串相加的结果", (done)=>{
+      // 步骤1，准备数据
+      const source$ = of("1", "2", "3");
+      
+      // 步骤2：执行函数
+      const result$ = source$.pipe(sum2);
+
+      // 步骤3： 检查结果
+      result$.subscribe(
+        value=>{
+          console.log(`sum2 test2, in next, value = ${value}`);
+          expect(value).toEqual("0123");
+          done();
+        },
+        err => {
+          console.log(`sum2 test2,  in error, err = ${JSON.stringify(err)}`);
+        },
+        ()=>{
+          console.log(`sum2 test2, in complete~~`);
+        }
+      )
+    });  
+
+
+    it("测试异步结果，测试设置超时时间", (done)=>{
+      // 步骤1，准备数据
+      // const source$ = of("1", "2", "3");
+      const source$ = timer(7000); // 7秒后才返回数据，但是框架在5秒没返回就会认为失败。
+      jest.setTimeout(8000); // 设置本条用例的超时时间，如果不设置会自动失败
+
+      // 步骤2：执行函数
+      const result$ = source$.pipe(sum2);
+
+      // 步骤3： 检查结果
+      result$.subscribe(
+        value=>{
+          console.log(`sum2 test2, in next, value = ${value}`);
+          expect(value).toEqual(0);
+          done();
+        },
+        err => {
+          console.log(`sum2 test2,  in error, err = ${JSON.stringify(err)}`);
+        },
+        ()=>{
+          console.log(`sum2 test2, in complete~~`);
+        }
+      )
+    });   
   })
 })
 
