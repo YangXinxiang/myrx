76f760e4be6723a7ca4e0be7a7c9cb21cfa831d5
yangxinxiang <yangxinxiang@100tal.com>
2021-01-24 17:05:42 +0800

    feat: 测试interval返回多个数据的场景. 基本数据的断言修改为toBe, 复杂数据的断言使用toEqual

--- test/sum2.test.js
+++ test/sum2.test.js
@@ -1,4 +1,5 @@
-import {range, of, timer} from "rxjs"
+import {range, of, timer, interval} from "rxjs";
+import {take} from "rxjs/operators";
 import { sum2 } from '../src/base/chapters/chapter-12/sum2';
 
 describe('index', () => {
@@ -15,7 +16,7 @@ describe('index', () => {
       result$.subscribe(
         value=>{
           console.log(`sum2 test, in next, value = ${value}`);
-          expect(value).toEqual(21);
+          expect(value).toBe(21);
           done();
         },
         err => {
@@ -38,7 +39,7 @@ describe('index', () => {
       result$.subscribe(
         value=>{
           console.log(`sum2 test2, in next, value = ${value}`);
-          expect(value).toEqual("0123");
+          expect(value).toBe("0123");
           done();
         },
         err => {
@@ -54,8 +55,8 @@ describe('index', () => {
     it("测试异步结果，测试设置超时时间", (done)=>{
       // 步骤1，准备数据
       // const source$ = of("1", "2", "3");
-      const source$ = timer(7000); // 7秒后才返回数据，但是框架在5秒没返回就会认为失败。
-      jest.setTimeout(8000); // 设置本条用例的超时时间，如果不设置会自动失败
+      const source$ = timer(1000); // 7秒后才返回数据，但是框架在5秒没返回就会认为失败。
+      // jest.setTimeout(8000); // 设置本条用例的超时时间，如果不设置会自动失败
 
       // 步骤2：执行函数
       const result$ = source$.pipe(sum2);
@@ -64,7 +65,7 @@ describe('index', () => {
       result$.subscribe(
         value=>{
           console.log(`sum2 test2, in next, value = ${value}`);
-          expect(value).toEqual(0);
+          expect(value).toBe(0);
           done();
         },
         err => {
@@ -76,5 +77,29 @@ describe('index', () => {
       )
     });   
   })
+
+
+
+  // 测试异步返回多个数据
+  describe("测试异步返回多个数据", ()=>{
+    it("测试interval返回多个数据的加和数据", (done)=>{
+      // 1, 准备数据
+      const source$ = interval(500).pipe(take(4));
+      // 2，执行函数
+      // const result$ = source$.pipe(sum2);
+      const rstArr = [];
+
+      source$.subscribe(
+        value => rstArr.push(value),
+        error => console.log(`sum3 test3,  in error, err = ${JSON.stringify(error)}`),
+        ()=>{
+          console.log(`sum3 test3, in complete~~rstArr = ${JSON.stringify(rstArr)}`);
+          expect(rstArr).toEqual([0,1,2,3]);
+          done();
+        }
+      )
+    })
+  })
+
 })
 
