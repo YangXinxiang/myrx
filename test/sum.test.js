70458d16f677d7d53fa8f344e8aeb2ce0494b4ca
yangxinxiang <yangxinxiang@100tal.com>
2021-01-26 23:39:06 +0800

    feat: jest单元测试入门用例

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,19 +1,25 @@
 import { sum } from '../src/util/sum';
 
-describe('index', () => {
+describe('测试求和模块', () => {
   describe('sum', () => {
     it('无参数时返回 0', () => {
-      expect(sum()).toEqual(0)
+      expect(0).toBe(0)
     })
-
+    /* */
+    it('无参数时返回 0', () => {
+      expect(sum()).toBe(0)
+    })
+    
     it('一个数时返回它本身', () => {
-      expect(sum(1)).toEqual(1)
-      expect(sum(2)).toEqual(2)
+      expect(sum(1)).toBe(1)
+      expect(sum(2)).toBe(2)
     })
-
+    
     it('多个数时返回它们的和', () => {
-      expect(sum(1, 2, 3)).toEqual(6)
+      expect(sum(1, 2, 3)).toBe(6)
     })
+    
   })
 })
+
 
